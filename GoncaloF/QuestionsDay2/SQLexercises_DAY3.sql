fd19f386c6d512403930c3704dcb6e087e1a1d32
GoncaloFelicio <goncalo.ag.felicio@outlook.com>
2022-03-24 11:15:04 +0100

    sqlexercises day3
    new: timestampdiff, convert, neat algorithm to get elapsed days from consecutive dates in groups

---
+++ GoncaloF/QuestionsDay2/SQLexercises_DAY3.sql
@@ -0,0 +1,34 @@
+USE classicmodels;
+
+-- Question 1 Number of days between 2 dates with datediff()
+SELECT 
+	customerNumber,
+	min(o.orderDate) as lastOrder,
+    max(o.orderDate) as lastOrder,
+    TIMESTAMPDIFF(day, min(o.orderDate), max(o.orderDate)) as daysElapsed,
+    TIMESTAMPDIFF(month, min(o.orderDate), max(o.orderDate)) as monthsElapsed,
+    TIMESTAMPDIFF(year, min(o.orderDate), max(o.orderDate)) as yearsElapsed
+FROM orders o
+GROUP BY customerNumber
+order by daysElapsed DESC
+;
+
+
+-- Question 2
+-- Neat way to calculate consecutive days elapsed 
+-- (IFNULL(datediff(o.orderDate,(SELECT MAX(oo.orderDate)
+-- 								FROM orders oo
+--                              WHERE oo.orderDate < o.orderDate
+--                              AND oo.customerNumber = o.customerNumber)),0) as daysElapsed
+SELECT
+	o.customerNumber,
+    count(o.orderDate) as numberOfOrders,
+	CONVERT (AVG(IFNULL(datediff(o.orderDate,(SELECT MAX(oo.orderDate)
+								FROM orders oo
+                                WHERE oo.orderDate < o.orderDate
+                                AND oo.customerNumber = o.customerNumber)),0)), UNSIGNED) as AVGdaysElapsedInt
+																			-- unsigned is for positive and 0 only
+FROM orders o
+GROUP BY o.customerNumber
+order by o.customerNumber
+;				
